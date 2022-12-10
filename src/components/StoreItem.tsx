a0c82b759259a4e7996e6f48dfb76225f6e039c1
Elya-Zh <elya7us@gmail.com>
2022-12-10 13:07:03 -0500

    shoppingContext is added

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
-import { Card } from "react-bootstrap"
+import { Button, Card } from "react-bootstrap"
+import { formatCurrency } from "../utilities/formatCurrency" 
 
 type StoreItemProps = {
     id:number,
@@ -8,17 +9,35 @@ type StoreItemProps = {
 }
 
 export function StoreItem({id, name, price, imgUrl}:StoreItemProps){
+
+    const quantity = 0;
     return(
-        <Card>
+        <Card className='h-100'>
       <Card.Img
       variant='top'
       src={imgUrl}
       height='200px'
       style={{objectFit:'cover'}}/>
       <Card.Body className='d-flex flex-column'>
-          <Card.Title className=''>
+          <Card.Title className='d-flex 
+          justify-content-between align-items-baseline mb-4'>
+          <span className='fs-2'>{name}</span>
+          <span className='ms-2 text-muted'>{formatCurrency(price)}</span>
 
           </Card.Title>
+          <div className="mt-auto">
+          {quantity=== 0 ? <Button className='w-100'>Add to cart</Button> : 
+          <div className='d-flex align-items-center flex-column' style={{gap:'.5rem'}}>
+              <div className='d-flex align-items-center justify-content-center'>
+                  <Button>-</Button>
+                  <div>
+                  <span className='fs-3'>{quantity}</span>
+                 </div>
+                  <Button>+</Button>
+              </div>
+              <Button variant='danger' size='sm'>Remove</Button>
+              </div>}
+          </div>
       </Card.Body>
         </Card>
     )
